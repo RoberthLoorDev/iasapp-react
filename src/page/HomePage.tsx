fde051ffa6037b3eb356416da90035fe38753dfe
RoberthLoorDev <dayvidlife@gmail.com>
2025-10-05 21:54:46 -0500

    add: shadcn config

--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -1,3 +1,10 @@
+import { Button } from "../components/ui/button";
+
 export default function HomePage() {
-     return <div className="">Hola mundo desde la Home page</div>;
+     return (
+          <div className="">
+               Hola mundo desde la Home page
+               <Button>Hola mundo</Button>
+          </div>
+     );
 }
